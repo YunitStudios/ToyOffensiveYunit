489f5e31337e2523ef314bcbd5b47b788c2c0bc0
Jevin <75545931+JevinLevin@users.noreply.github.com>
2026-02-10 20:28:41 +0000

    end screen leaderboard
    Added leaderboard to end screen, also tweaked tabs to support a title

--- Assets/Scripts/UI/Core/MenuTab.cs
+++ Assets/Scripts/UI/Core/MenuTab.cs
@@ -1,6 +1,17 @@
+using EditorAttributes;
+using TMPro;
 using UnityEngine;
 
 public class MenuTab : MenuContainer
 {
+    [Title("\n<b><color=#fb80ff>Tab Attributes", 15, 5, false)] 
+    [SerializeField] private TMP_Text tabTitle;
 
+    public override void ToggleContent(bool value)
+    {
+        base.ToggleContent(value);
+
+        if(tabTitle)
+            tabTitle.text = button.GetText;
+    }
 }
