9086f500b7170a3859f3b4bc529d1253b6da55c5
Katie <87566534+KatieGames@users.noreply.github.com>
2026-02-26 18:52:59 +0000

    Added pickups
    https://docs.google.com/document/d/1t2rY7htGNqohC1ys2HrOUXPyDCHDFH3NyRS08zdnfBk/edit?usp=sharing

---
+++ Assets/Scripts/Player/Inventory/PickupSystem/PickupDatabaseSO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Inventory.PickupSystem
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Pickup Database")]
+    public class PickupDatabaseSO : ScriptableObject
+    {
+        [System.Serializable]
+        public struct PickupDatabaseEntry
+        {
+            public PickupObjectType type;
+            public AmmoType ammoType;   // only relevant if type == Ammo
+            public GameObject prefab;
+            public int value;
+        }
+
+        [SerializeField] private List<PickupDatabaseEntry> entries;
+
+        private Dictionary<PickupObjectType, PickupObject> lookup;
+
+        public PickupDatabaseEntry? GetRandomPickup()
+        {
+            if (entries == null || entries.Count == 0)
+                return null;
+
+            int index = Random.Range(0, entries.Count);
+            return entries[index];
+        }
+    }
+}
