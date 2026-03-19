d54d7235807035d575db50533e1044f8febb1abc
Katie <katiedev1273@gmail.com>
2026-03-19 15:19:57 +0000

    fix always showing exfil

--- Assets/Scripts/UI/HUD/DirectionalMarkers/DirectionalMarkerComponent.cs
+++ Assets/Scripts/UI/HUD/DirectionalMarkers/DirectionalMarkerComponent.cs
@@ -7,17 +7,41 @@ public class DirectionalMarkerComponent : MonoBehaviour
     [Header("Parameters")]
     [SerializeField] private MarkerTypes markerType;
     [SerializeField] private bool hasWorldSprite = false;
+    [SerializeField] private bool showAlways = true;
     
     [Header("References")]
     [SerializeField] private GameObject imageObject;
+
+    [SerializeField] private VoidEventChannelSO onMissionCompleted;
+
+    private void OnEnable()
+    {
+        if(onMissionCompleted != null)
+            onMissionCompleted.OnEventRaised += ShowImage;
+    }
+
+    private void OnDisable()
+    {
+        if(onMissionCompleted != null)
+            onMissionCompleted.OnEventRaised -= ShowImage;
+    }
+
     void Start()
     {
+        if (!showAlways)
+            imageObject.SetActive(false);
         if(markerType == MarkerTypes.Target)
             RadialHUDDisplay.SpawnLiveMarker?.Invoke(transform, markerType);
         else
         {
             RadialHUDDisplay.SpawnMarker?.Invoke(transform.position, markerType);
         }
+    }
+
+    private void ShowImage()
+    {
+        if(!showAlways)
+            imageObject.SetActive(true);
     }
 
     private void Update()
