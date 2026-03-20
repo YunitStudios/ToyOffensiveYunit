b1081dd26f99d543a9b2a54dcdfae942fe8eae23
Lady_Synestia <katiedev1273@gmail.com>
2026-03-20 18:59:56 +0000

    Fix level not letting you move on start

--- Assets/Scripts/Utility/LevelStarter.cs
+++ Assets/Scripts/Utility/LevelStarter.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class LevelStarter : MonoBehaviour
 {
+    [SerializeField] private VoidEventChannelSO pauseEvent;
     // Starts the actual game after loading a game scene
     void Start()
     {
         if(!GameManager.Instance.Ingame)
+        {
             GameManager.Instance.StartLevel();
-        
+            pauseEvent?.Invoke();
+        }        
     }
     
 }
