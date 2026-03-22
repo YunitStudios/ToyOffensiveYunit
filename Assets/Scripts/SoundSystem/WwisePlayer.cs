5ff9ba1c82c3d4fdea578259d9c81429916f89f5
Jevin <75545931+JevinLevin@users.noreply.github.com>
2026-03-22 23:51:39 +0000

    reworked footstep sounds

--- Assets/Scripts/SoundSystem/WwisePlayer.cs
+++ Assets/Scripts/SoundSystem/WwisePlayer.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SoundSystem
 {
     public class WwisePlayer : MonoBehaviour
     {
+        [SerializeField] private FootData[] feet;
+        [SerializeField] private float footSoundThreshold = 0;
+        [SerializeField] private SoundDataSO footstep;
+
+        [Serializable]
+        private class FootData
+        {
+            public Transform transform;
+            public bool onGround;
+        }
+        
         public void PlaySound(SoundDataSO SoundData)
         {
             if (SoundData == null || string.IsNullOrEmpty(SoundData.WwiseName))
@@ -12,7 +24,7 @@ namespace SoundSystem
                 Debug.LogWarning("SoundData or WwiseName is not set.");
                 return;
             }
-
+            
             // use main listener if 2D, otherwise self-emitting
             // TODO: see if this actually works and is useful
             GameObject target = SoundData.Is2D ? Camera.main.gameObject : this.gameObject;
@@ -32,6 +44,36 @@ namespace SoundSystem
             }
             
             // Debug.Log(SoundData.WwiseName);
+        }
+
+        private void Update()
+        {
+            PlayFootsteps();
+        }
+
+        private void PlayFootsteps()
+        {
+            // Check if any feet are below the Y threshold
+            foreach (var foot in feet)
+            {
+
+                Vector3 playerPosition = transform.position;
+                Vector3 footPosition = foot.transform.position;
+                float difference = footPosition.y - playerPosition.y;
+                print(difference);
+                
+                if(foot.onGround && difference >= footSoundThreshold)
+                {
+                    foot.onGround = false;
+                }
+                if (!foot.onGround && difference < footSoundThreshold)
+                {
+                    PlaySound(footstep);
+                    foot.onGround = true;
+                }
+
+            }
+            
         }
     }
 }
